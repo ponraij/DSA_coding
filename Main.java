038e0c5e3a368a27cd0d2bdd8f30617b4ac889f9
ponraj <109679442+ponraij@users.noreply.github.com>
2024-01-18 11:23:14 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,28 +1,28 @@
 import java.util.*;
 public class Main
 {
-	public static void main(String[] args) {
-	    Scanner sc=new Scanner(System.in);
-	    String str=sc.nextLine();
-	    char[] a=str.toCharArray();
-	    int n=str.length();
-	    int s=n-1;
-	    int e=n-1;
-	    for(int i=n-1;i>=0;i--){
-	        if(a[i]!=' ' ){
-	            s--;
-	        }
-	        else{
-	            for(int j=s;j<=e;j++){
-	                System.out.print(a[j]);
-	            }
-	            System.out.print(" ");
-	            e=s;
-	        }
-	    }
-	    //System.out.println(s + " " +e);
-	    for(int i=s-1;i<e;i++){
-	        System.out.print(a[i]);
-	    }
-	    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        char[] a=str.toCharArray();
+        int n=str.length();
+        int s=n-1;
+        int e=n-1;
+        for(int i=n-1;i>=0;i--){
+            if(a[i]!=' ' ){
+                s--;
+            }
+            else{
+                for(int j=s+1;j<=e;j++){
+                    System.out.print(a[j]);
+                }
+                System.out.print(" ");
+                e = s-1;
+                s = e;
+            }
+        }
+        for(int j=s+1;j<=e;j++){
+            System.out.print(a[j]);
+        }
+    }
 }
